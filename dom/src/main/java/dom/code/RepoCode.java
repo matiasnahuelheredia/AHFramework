4a29d252ecb0fa912b28f6ef456993c111847d89
matias nahuel heredia <matiasnahuelheredia@gmail.com>
2013-06-23 23:06:13 -0300

    add code repository

---
+++ dom/src/main/java/dom/code/RepoCode.java
@@ -0,0 +1,12 @@
+package dom.code;
+
+import org.apache.isis.applib.AbstractFactoryAndRepository;
+import org.apache.isis.applib.fixtures.AbstractFixture;
+
+public class RepoCode extends AbstractFactoryAndRepository {
+
+	public String codigo() 
+	{
+		return "hola";
+	}
+}
