4d6b0c0b1682171949d92d2514c6a01ae396dfb1
Matias Nahuel Heredia <matias@informaticos.com>
2013-08-24 06:28:02 -0300

    add code generator for arduino LEONARDO

--- dom/src/main/java/dom/code/ServiceCodeGen.java
+++ dom/src/main/java/dom/code/ServiceCodeGen.java
@@ -15,4 +15,26 @@ public class ServiceCodeGen {
 		    }
 			return code;
 	}
+	
+	
+	public static String generateCodeLeonardo (String code,List<Properties> prop)
+	{
+		    for (int i=0;i<prop.size();i++)
+		    {
+		    	String replaceValue = prop.get(i).getValue();
+		    	System.out.println("holichus leonardo");
+		    	if (prop.get(i).getReplaceValue().contains("keyboard"))
+		    	{
+		    		System.out.println("leonardo contiene keyboard");
+		    		
+		    		replaceValue =	replaceValue.replace(":", "\");"+(char)13+"Keyboard.write(62);"+(char)13+"Keyboard.print(\"");
+		    		replaceValue =	replaceValue.replace("/", "\");"+(char)13+"Keyboard.write(38);"+(char)13+"Keyboard.print(\"");
+		    		replaceValue =	replaceValue.replace("+", "\");"+(char)13+"Keyboard.write(93);"+(char)13+"Keyboard.print(\"");
+		    		replaceValue = "Keyboard.print(\""+ replaceValue;
+		    		replaceValue = replaceValue+"\");"+(char)13+"Keyboard.println(\"\");";;
+		    	}
+			code = code.replace(prop.get(i).getReplaceValue(), replaceValue);
+		    }
+			return code;
+	}
 }
